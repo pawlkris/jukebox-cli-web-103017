41b8336d042b797e003a6b874c1b0b1c69680168
Paul Kristapovich <paulkristapovich@gmail.com>
2017-10-24 15:26:46 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+puts("Please enter a command:")
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,54 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts("I accept the following commands:")
+  puts("- help : displays this help message")
+  puts("- list : displays a list of songs you can play")
+  puts("- play : lets you choose a song to play")
+  puts("- exit : exits this program")
+end
+
+def list(songs)
+  songs.each_with_index do |song, i|
+    puts("#{i+1}. #{song}")
+    i =+ 1
+  end
+end
+
+def play(songs)
+  puts("Please enter a song name or number:")
+  song = gets.chomp
+  if songs.include?(song)
+    puts("Playing #{song}")
+  elsif
+    (1..9).include?(song)
+    puts("Playing #{songs[song+1]}")
+  else
+    puts("Invalid input, please try again")
+  end
+end
+
+def exit_jukebox
+  puts("Goodbye")
+end
+
+def run(songs)
+  help
+  until command == "exit"
+    command = ""
+    puts("Please enter a command:")
+    command = gets.chomp
+    if command == "list"
+      list(songs)
+    elsif command == "play"
+      play(songs)
+    elsif command == "help"
+      help
+    elsif command == "exit"
+      exit_jukebox
+    else
+      puts("That command is not valid. Please enter a different command:")
+    end
+  end
+end
